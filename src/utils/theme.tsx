4aba92bc7e9aef138549f0fab8274aea7683b33e
hai.nguyen <congtuhot9.9@gmail.com>
2025-08-14 01:11:24 +0700

    fix: hiệu ứng glass bị chậm khi chuyển theme -> remove <motion>

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,8 +1,7 @@
 import { MediaHTMLAttributes, useEffect, useState } from "react";
 import { ELECTRON_EVENTS } from "../constants";
-import { motion } from "framer-motion";
 import darkVideo from "../assets/video-corgi-dark.mp4";
-import lightVideo from "../assets/video-cat-light.mp4";
+import lightVideo from "../assets/video-corgi-light-2.mp4";
 
 export const Theme = {
   system: "system",
@@ -42,7 +41,7 @@ export function applyTheme(theme: string) {
   }
 }
 
-const duration = 500;
+const duration = 300;
 
 const videoBase: Partial<MediaHTMLAttributes<HTMLVideoElement>> = {
   style: {
@@ -56,60 +55,53 @@ const videoBase: Partial<MediaHTMLAttributes<HTMLVideoElement>> = {
   muted: true,
   loop: true,
   playsInline: true,
-  className: "fixed inset-0 pointer-events-none z-[-999]",
-};
-
-const variants = {
-  visible: {
-    opacity: 1,
-    transition: { when: "beforeChildren" },
-  },
-  hidden: { opacity: 0, transition: { when: "afterChildren" } },
+  className: "fixed inset-0 pointer-events-none",
 };
 
 export function ThemeBackground() {
   const theme = useSystemTheme();
 
-  const backgroundImage =
-    theme === Theme.dark
-      ? "https://i.pinimg.com/1200x/b8/1d/48/b81d48a7f323e4c85775f41114cb3b65.jpg"
-      : "https://i.pinimg.com/1200x/8f/b1/38/8fb138947d35d4bf2493904484a4a77f.jpg";
+  // const backgroundImage =
+  //   theme === Theme.dark
+  //     ? "https://i.pinimg.com/1200x/b8/1d/48/b81d48a7f323e4c85775f41114cb3b65.jpg"
+  //     : "https://i.pinimg.com/1200x/8f/b1/38/8fb138947d35d4bf2493904484a4a77f.jpg";
 
   return (
     <>
       {/* fall back background color */}
-      <div
+      {/* <div
         className="fixed inset-0 pointer-events-none z-[-99999]"
         style={{
           background: "light-dark(#ffffff, #000000)",
-          transition: `background-color ${duration / 1000}s ease`,
+          transition: `background-color ${duration}ms ease`,
         }}
-      />
+      /> */}
+      {/* <div
+        className="z-[-999] fixed inset-0 pointer-events-none"
+        style={{
+          transition: `background ${duration}ms ease`,
+          background: `url('${backgroundImage}') center/cover`,
+        }}
+      ></div> */}
+
       <div
         className="z-[-999] fixed inset-0 pointer-events-none"
         style={{
-          transition: `background ${duration / 1000}s ease`,
-          background: `url('${backgroundImage}') center/cover`,
+          transition: `opacity ${duration}ms ease`,
+          background: `url('https://i.pinimg.com/1200x/8f/b1/38/8fb138947d35d4bf2493904484a4a77f.jpg') center/cover`,
+          opacity: theme === Theme.light ? 1 : 0,
         }}
       ></div>
 
-      {/* Dark theme video */}
-      <motion.div
-        initial={theme === Theme.dark ? variants.visible : variants.hidden}
-        animate={theme === Theme.dark ? variants.visible : variants.hidden}
-        exit={theme === Theme.dark ? variants.hidden : variants.visible}
-        transition={{ duration }}
+      <div
+        className="z-[-999] fixed inset-0 pointer-events-none"
+        style={{
+          transition: `opacity ${duration}ms ease`,
+          opacity: theme === Theme.dark ? 1 : 0,
+        }}
       >
         <video {...videoBase} src={darkVideo}></video>
-      </motion.div>
-      {/* <motion.div
-        initial={theme === Theme.light ? variants.visible : variants.hidden}
-        animate={theme === Theme.light ? variants.visible : variants.hidden}
-        exit={theme === Theme.light ? variants.hidden : variants.visible}
-        transition={{ duration }}
-      >
-        <video {...videoBase} src={lightVideo}></video>
-      </motion.div> */}
+      </div>
     </>
   );
 }
